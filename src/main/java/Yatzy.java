b3d900053f4f2b63df881b14f4c0ff437188e0d4
yohan <damien.lequeux@gmail.com>
2019-10-09 17:26:04 +0200

    revert commit of refactoring full house

--- src/main/java/Yatzy.java
+++ src/main/java/Yatzy.java
@@ -130,31 +130,41 @@ public class Yatzy {
 
     public int largeStraight() {
         int[] tallies = diceValueCounter(roll);
+
         if (tallies[1] == 1 &&
                 tallies[2] == 1 &&
                 tallies[3] == 1 &&
-                tallies[4] == 1 &&
-                tallies[5] == 1)
+                tallies[4] == 1
+                && tallies[5] == 1)
             return 20;
         return 0;
     }
 
     public int fullHouse() {
-        int total = 0;
+        boolean _2 = false;
+        int i;
+        int _2_at = 0;
+        boolean _3 = false;
+        int _3_at = 0;
+
         int[] tallies = diceValueCounter(roll);
 
-        for (int diceValue = TOTAL_DICE_FACES; diceValue >= 1; diceValue--) {
-            if (tallies[diceValue - 1] >= 2) {
-
-                for (diceValue = TOTAL_DICE_FACES; diceValue >= 1; diceValue--) {
-                    if (tallies[diceValue - 1] >= 3) {
-
-                        return sumDiceValue();
-                    }
-                }
+        for (i = 0; i != 6; i += 1)
+            if (tallies[i] == 2) {
+                _2 = true;
+                _2_at = i + 1;
             }
-        }
-        return total;
+
+        for (i = 0; i != 6; i += 1)
+            if (tallies[i] == 3) {
+                _3 = true;
+                _3_at = i + 1;
+            }
+
+        if (_2 && _3)
+            return _2_at * 2 + _3_at * 3;
+        else
+            return 0;
     }
 
 }
